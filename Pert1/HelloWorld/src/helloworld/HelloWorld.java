102377f35a43c8538883efc62930ea6be09e5d12
VTanujaya <vtanujaya6@gmail.com>
2022-01-13 21:47:02 +0700

    Update HelloWorld.java

--- Pert1/HelloWorld/src/helloworld/HelloWorld.java
+++ Pert1/HelloWorld/src/helloworld/HelloWorld.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package helloworld;
 
 /**
@@ -10,9 +6,6 @@ package helloworld;
  */
 public class HelloWorld {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        System.out.println("Halo Dunia Satu Keluarga!");
     }
