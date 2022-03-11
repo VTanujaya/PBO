f9c0e8f14521f65585e2dd77efb7a1ef538a5839
VTanujaya <vtanujaya6@gmail.com>
2022-03-11 16:16:53 +0700

    Update HelloWorld.java

--- Pert1/HelloWorld/src/helloworld/HelloWorld.java
+++ Pert1/HelloWorld/src/helloworld/HelloWorld.java
@@ -7,7 +7,8 @@ package helloworld;
 public class HelloWorld {
 
     public static void main(String[] args) {
-       System.out.println("Halo Dunia Satu Keluarga!");
+        System.out.println("Halo Dunia Satu Keluarga!");
+        System.out.println("HeeHee");
     }
-    
+
 }
